c4679d6684ad36f1bc4deef39f8023a859d7b754
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-13 11:20:56 +0100

    Add form structure

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,7 +3,12 @@ import React from "react";
 export default function Form() {
   return (
     <form>
-      <p>this will become a form</p>
+      <h2>Add new Activity:</h2>
+      <label htmlFor="inputName">Name: </label>
+      <input type="text" id="inputName" className="inputName" />
+      <label htmlFor="checkbox">Good-weather activity:</label>
+      <input type="checkbox" />
+      <input value="submit" type="submit" />
     </form>
   );
 }
