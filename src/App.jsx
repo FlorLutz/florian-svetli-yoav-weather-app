53c4a9166be7063dfff220bfd31d47643c0dd9a6
Yoav Hainebach <yoavhainebach@gmail.com>
2024-03-13 12:02:40 +0100

    Make the form functioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
+import { uid } from "uid";
 import "./App.css";
 import Form from "./components/Form/Form.jsx";
 import Weather from "./components/Weather/Weather.jsx";
 import List from "./components/List/List.jsx";
 
 export default function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    const newActivity = {
+      id: uid(),
+      name: data.inputName,
+      isGoodWeather: data.checkbox ? true : false,
+    };
+    console.log(newActivity);
+  }
+
   return (
     <>
       <Weather />
       <List />
-      <Form />
+      <Form onAddActivity={handleSubmit} />
     </>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({ onAddActivity }) {
   return (
-    <form>
+    <form onSubmit={onAddActivity}>
       <h2>Add new Activity:</h2>
       <label htmlFor="inputName">Name: </label>
-      <input type="text" id="inputName" className="inputName" />
+      <input
+        type="text"
+        id="inputName"
+        className="inputName"
+        name="inputName"
+      />
       <label htmlFor="checkbox">Good-weather activity:</label>
-      <input type="checkbox" />
+      <input type="checkbox" id="checkbox" name="checkbox" />
       <input value="submit" type="submit" />
     </form>
   );
