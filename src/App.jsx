93d3d9d8d1dbb24b3f0243157f5ac01fc1a48b8f
Florian Lutz <florian.lutz@ymail.com>
2024-03-14 15:09:03 +0100

    add default and suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,26 @@ import List from "./components/List/List.jsx";
 export default function App() {
   const inititialActivities = [
     {id: uid(), name: "Do yoga in your closest park", isGoodWeather: true},
+    {id: uid(), name: "Chill in the park listening to <a href='https://open.spotify.com/playlist/0jGxTY0oQCIdzvteY0ygGG'>Good Weather Vibes</a>", isGoodWeather: true},
     {id: uid(), name: "Go to the lake and get some tan", isGoodWeather: true},
-    {id: uid(), name: "Go stargazing", isGoodWeather: true},
     {id: uid(), name: "Time to head for a Biergarten", isGoodWeather: true},
     {id: uid(), name: "Learn how to rollerskate", isGoodWeather: true},
     {id: uid(), name: "Go to the gym", isGoodWeather: false},
-    {id: uid(), name: "Listen to https://open.spotify.com/intl-de/artist/4AZab8zo2nTYd7ORDmQu0V The Dead Weather</a>", isGoodWeather: false},
+    {id: uid(), name: "Listen to <a href='https://open.spotify.com/intl-de/artist/4AZab8zo2nTYd7ORDmQu0V'>The Dead Weather</a>", isGoodWeather: false},
+    {id: uid(), name: "Learn and play a new card game", isGoodWeather: false},
+    {id: uid(), name: "Stare at the wall", isGoodWeather: false},
+    {id: uid(), name: "Paint the first thing you see", isGoodWeather: false},
+  ]
+  const suggestedGoodWeatherActivities = [
+    {id: uid(), name: "Go stargazing", isGoodWeather: true},
+    {id: uid(), name: "Do yoga in your closest park", isGoodWeather: true},
+    {id: uid(), name: "Go to the lake and get some tan", isGoodWeather: true},
+    {id: uid(), name: "Time to head for a Biergarten", isGoodWeather: true},
+    {id: uid(), name: "Learn how to rollerskate", isGoodWeather: true}
+  ]
+  const suggestedBadWeatherActivities = [
+    {id: uid(), name: "Go to the gym", isGoodWeather: false},
+    {id: uid(), name: "Listen to <a href='https://open.spotify.com/intl-de/artist/4AZab8zo2nTYd7ORDmQu0V'>The Dead Weather</a>", isGoodWeather: false},
     {id: uid(), name: "Learn and play a new card game", isGoodWeather: false},
     {id: uid(), name: "Stare at the wall", isGoodWeather: false},
     {id: uid(), name: "Paint the first thing you see", isGoodWeather: false},
@@ -23,7 +37,9 @@ export default function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: inititialActivities,
   });
-  console.log("activities", activities);
+
+  const [suggestedGWA, setSuggestedGWA] = useState(suggestedGoodWeatherActivities);
+  const [suggestedBWA, setSuggestedBWA] = useState(suggestedBadWeatherActivities);
 
   const [weatherData, setWeatherData] = useState({});
   useEffect(() => {
@@ -54,6 +70,25 @@ export default function App() {
     event.target.reset();
   }
 
+  function handleSuggest () {
+    let newActivity;
+    if (weatherData.isGoodWeather && suggestedGWA) {
+      newActivity = suggestedGWA[0]
+      suggestedGWA.shift()
+      setSuggestedGWA(suggestedGWA)
+      console.log("suggestedGWA", suggestedGWA);
+
+    }
+    else if (!weatherData.isGoodWeather && suggestedBWA) {
+      newActivity = suggestedBWA[0]
+      suggestedBWA.shift()
+      setSuggestedBWA(suggestedBWA)
+      console.log("suggestedBWA", suggestedBWA);
+    }
+    
+    newActivity ? setActivities([newActivity, ...activities]) : alert("I just ran out of suggestions for that weather, sorry 🥺");
+  }
+
   const filteredActivities = activities.filter(
     (activity) => activity.isGoodWeather === weatherData.isGoodWeather
   );
@@ -75,7 +110,7 @@ export default function App() {
         filteredActivities={filteredActivities}
         onDeleteActivity={handleDeleteActivity}
       />
-      <Form onAddActivity={handleSubmit} />
+      <Form onAddActivity={handleSubmit} onSuggestActivity={handleSuggest}/>
     </>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Form({ onAddActivity }) {
+export default function Form({ onAddActivity, onSuggestActivity }) {
   return (
     <form onSubmit={onAddActivity}>
       <h2>Add new Activity:</h2>
@@ -14,6 +14,9 @@ export default function Form({ onAddActivity }) {
       <label htmlFor="checkbox">Good-weather activity:</label>
       <input type="checkbox" id="checkbox" name="checkbox" />
       <input value="submit" type="submit" />
+      <br/>
+      <p>Don't feel inspired today?</p>
+      <input onClick= {onSuggestActivity} value="get suggestion" type="button" />
     </form>
   );
 }
