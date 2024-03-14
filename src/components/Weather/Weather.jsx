99c7a42753220cf4ebee787c7e86a6ddf7ea0b54
svetlivaksevski <svetlivaksevski@gmail.com>
2024-03-14 19:57:10 +0100

    Add loading to weather.jsx

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Weather({ temperature, condition }) {
-  return <p>{`${condition} ${temperature} °C`}</p>;
+  if (typeof temperature !== "undefined" && typeof condition !== "undefined") {
+    return <p>{`${condition} ${temperature} °C`}</p>;
+  } else {
+    return <p>Loading...</p>;
+  }
 }
